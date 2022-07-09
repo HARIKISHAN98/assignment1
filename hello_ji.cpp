28e967c8eb16b679e458cd620d656f79bc35922d
1Harikishan2 <92172801+1Harikishan2@users.noreply.github.com>
2022-07-09 13:22:38 +0530

    Add files via upload

---
+++ hello_ji.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Hello ji complete ho gya  "<<endl;
+// cout<<
+char ch='3.8';
+printf("%d\n",ch);
+// printf("%d\n",'A');
+printf("%c\n",ch);
+
+cout<< ch <<endl;
+
+    return 0;
+}
